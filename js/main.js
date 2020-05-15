f9cc1f3a86e274d38c5dee1278fda50401b4b961
edbrmm <ricardo.miyoshi@gmail.com>
2020-05-15 18:01:07 -0300

    Aula 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,68 @@
+function clicou() {
+  document.getElementById('agradecimento').innerHTML =
+    '<b>Obrigado por clicar</b>';
+  //console.log(document.getElementById('agradecimento'));
+  //alert('Obrigado por clicar');
+}
+
+function redirecionar() {
+  //window.open('https://web.digitalinnovation.one/home');
+  window.location.href = 'https://web.digitalinnovation.one/home';
+}
+
+function trocar(elemento) {
+  elemento.innerHTML = 'Obrigado por passar o mouse';
+  //document.getElementById('mousemove').innerHTML =
+  ('Obrigado por passar o mouse');
+  //alert('Trocar texto');
+}
+
+function voltar(elemento) {
+  elemento.innerHTML = 'Passe o mouse aqui';
+  //document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui';
+  //alert('Trocar texto');
+}
+
+function load() {
+  alert('Página Carregada');
+}
+
+function funcaoChange(elemento) {
+  console.log(elemento.value);
+}
+
+/*
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+function validaIdade(idade) {
+  var validar;
+  if (idade >= 18) {
+    validar = true;
+  } else {
+    validar = false;
+  }
+  return validar;
+}
+
+var idade = prompt('Qual sua idade');
+console.log(validaIdade(idade));
+*/
+/*
+function setReplace(frase, nome, novo_nome) {
+  return frase.replace(nome, novo_nome);
+}
+*/
+//alert(soma(5, 10));
+//alert(setReplace('Vai Japão', 'Japão', 'Brasil'));
+
+/*
 var d = new Date();
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
-
-var lista  = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
-console.log(lista[2]);
-
+*/
 /*
 var count;
 for (count = 0; count <= 5; count++) {
