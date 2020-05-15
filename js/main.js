0068a6c9e898413ca8973b22e734c876cc0a1ca8
edbrmm <ricardo.miyoshi@gmail.com>
2020-05-15 11:57:51 -0300

    Aula 1

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+var lista  = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
+console.log(lista[2]);
+
+/*
+var count;
+for (count = 0; count <= 5; count++) {
+  alert(count);
+}
+*/
+/*var count = 0;
+while (count < 5) {
+  console.log(count);
+  count++;
+}
+*/
+/*
+var idade = prompt('Qual a sua idade');
+//var idade = 18;
+if (idade >= 18) {
+  alert('maior de idade');
+} else {
+  alert('menor de idade');
+}
+*/
+/*
+var frutas = [
+  { nome: 'maça', cor: 'vermelha' },
+  { nome: 'uva', cor: 'roxa' },
+];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = { nome: 'maça', cor: 'vermelha' };
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ['maça', 'pêra', 'laranja'];
+//lista.push('uva');
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(' - '));
+
+/*
+var nome = 'Ricardo Miyoshi';
+var idade = 50;
+var idade2 = 10;
+var frase = 'Japão é o melhor time do mundo';
+
+//alert(nome + ' tem ' + idade + ' anos');
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace('Japão', 'Brasil'));
+alert(frase.replace('Japão', 'Brasil'));
+console.log(frase);
+*/
